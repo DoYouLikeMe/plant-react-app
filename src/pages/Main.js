06fdf4049c3f1324bf9d2782e4e7132941acdab3
Alexey Voronin <avoronsib@gmail.com>
2023-07-30 15:40:09 +0300

    add: main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,3 +1,24 @@
+import Leafs from "@/assets/images/leafs.png";
+
 export const Main = () => {
-  return <div>Главная страница</div>;
+  return (
+    <section className="main">
+      <div className="container">
+        <div className="main__wrapper">
+          <img className="main__image" src={Leafs} alt="Leafs" />
+          <div className="main__content">
+            <h1 className="section-heading main__heading">
+              We grow <span class="green-emphasizing">plants</span> and give you
+              oxygen
+            </h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
