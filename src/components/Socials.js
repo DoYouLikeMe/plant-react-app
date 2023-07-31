7d1bf5e53355e213a3b92441e7640cd7f33ffb2a
Alexey Voronin <avoronsib@gmail.com>
2023-07-31 16:33:34 +0300

    add: footer

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,11 +1,29 @@
 import {ReactComponent as InstIcon} from "@/assets/images/icons/inst.svg";
+import {ReactComponent as FbIcon} from "@/assets/images/icons/fb.svg";
+import {ReactComponent as TwIcon} from "@/assets/images/icons/tw.svg";
+import {ReactComponent as PinterestIcon} from "@/assets/images/icons/pinterest.svg";
 
 export const Socials = () => {
   return (
     <ul className="social-list">
       <li className="social-list__item">
-        <a href="instagram.com">
-          <InstIcon />
+        <a class="social-list__link" href="instagram.com">
+          <InstIcon className="social-list__icon" />
+        </a>
+      </li>
+      <li className="social-list__item">
+        <a class="social-list__link" href="instagram.com">
+          <FbIcon className="social-list__icon" />
+        </a>
+      </li>
+      <li className="social-list__item">
+        <a class="social-list__link" href="instagram.com">
+          <TwIcon className="social-list__icon" />
+        </a>
+      </li>
+      <li className="social-list__item">
+        <a class="social-list__link" href="instagram.com">
+          <PinterestIcon className="social-list__icon" />
         </a>
       </li>
     </ul>
